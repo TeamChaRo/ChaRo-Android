04a555898cbbcfab140827ea4d0a096e2976c39e
hansh0101 <hansh0101@naver.com>
2021-07-07 04:49:57 +0900

    #4 [FEAT] : 상세보기 뷰 출발지, 경유지, 목적지 / 지도 / 주차공간 구현

--- app/src/main/java/com/example/charo_android/DetailActivity.kt
+++ app/src/main/java/com/example/charo_android/DetailActivity.kt
@@ -3,6 +3,7 @@ package com.example.charo_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.charo_android.databinding.ActivityDetailViewBinding
+import com.skt.Tmap.TMapView
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailViewBinding
@@ -11,6 +12,9 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        val tMapView = TMapView(this)
+        val tMapViewContainer = binding.clDetailMapview
+        tMapView.setSKTMapApiKey("l7xx94a7679a3e1d41a782105327ae7af1cd")
+        tMapViewContainer.addView(tMapView)
     }
 }
