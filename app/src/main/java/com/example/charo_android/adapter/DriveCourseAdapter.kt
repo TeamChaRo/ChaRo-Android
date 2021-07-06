96dc1dc3ee760654301717f93ec6c32310e76587
hansh0101 <hansh0101@naver.com>
2021-07-06 15:59:10 +0900

    #4 [FEAT] : 상세보기 뷰 드라이브 코스 지역 구성

---
+++ app/src/main/java/com/example/charo_android/adapter/DriveCourseAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.charo_android.adapter
+
+import android.app.Activity
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.example.charo_android.data.DriveCourseItem
+
+class DriveCourseAdapter (
+    private val activity: Activity,
+    private val context: Context,
+    private val items: MutableList<DriveCourseItem>
+) : RecyclerView.Adapter<DriveCourseAdapter.DriveCourseAdapterVH>() {
+
+
+
+}
---
+++ app/src/main/java/com/example/charo_android/data/DriveCourseItem.kt
@@ -0,0 +1,5 @@
+package com.example.charo_android.data
+
+data class DriveCourseItem(
+    var city: String,
+)
