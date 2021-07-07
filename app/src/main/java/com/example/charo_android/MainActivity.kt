8aaf5566df1d718cc5ca3b83fb449dfcade7995b
Han Jinhee <h.wlsgml19@gmail.com>
2021-07-08 02:21:55 +0900

    [ADD,MODIFY] file

--- app/src/main/java/com/example/charo_android/MainActivity.kt
+++ app/src/main/java/com/example/charo_android/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.charo_android
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.charo_android.databinding.ActivityMainBinding
+import com.example.charo_android.ui.write.WriteActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +33,16 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        binding.btnWrite.setOnClickListener {
+            startActivityWrite()
+        }
     }
+
+    fun startActivityWrite() {
+        val intent = Intent(this@MainActivity, WriteActivity::class.java)
+        startActivity(intent)
+    }
+
 }
