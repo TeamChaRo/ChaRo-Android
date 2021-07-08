0f8992d924fbe835497313f820cced79cce24e2a
hansh0101 <hansh0101@naver.com>
2021-07-09 01:53:25 +0900

    #14 [FEAT] : 상세보기 뷰 코스설명 텍스트 길이 표시 함수 구현 및 selector 활용하여 이미지 바꾸는 함수 구현

--- app/src/main/java/com/example/charo_android/DetailActivity.kt
+++ app/src/main/java/com/example/charo_android/DetailActivity.kt
@@ -1,10 +1,17 @@
 package com.example.charo_android
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import com.example.charo_android.databinding.ActivityDetailViewBinding
+import com.example.charo_android.databinding.ItemDetailImageBinding
 import com.skt.Tmap.TMapView
 
 class DetailActivity : AppCompatActivity() {
@@ -22,42 +29,100 @@ class DetailActivity : AppCompatActivity() {
         tMapView.setSKTMapApiKey("")
         tMapViewContainer.addView(tMapView)
 
-//        initDetailImageItem()
-//        viewpagerDetailImageOnClickEvent()
-//        checkImgDetailViewpagerPrevNext()
-//        imgDetailViewpagerPrevOnClickEvent()
+        // 뷰페이저의 초기 데이터 삽입
+        initDetailImageItem()
+        // 드라이브 상세설명 글자수 표시
+        showDriveCourseInformationTextLenght()
+        // 좋아요 클릭 이벤트
+        imgDetailHeartOnClickEvent()
+        // 저장 클릭 이벤트
+        imgDetailSaveOnClickEvent()
+        // 주소 복사
+        imgDetailCopyOnClickEvent()
+
+        test()
     }
 
-//    private fun initDetailImageItem() {
-//        binding.viewpagerDetailImage.adapter = detailViewPagerAdapter
-//        detailViewPagerAdapter.imageList.addAll(LocalDetailViewpagerImageDataSource().fetchData())
-//        detailViewPagerAdapter.notifyDataSetChanged()
-//    }
-//
-//    private fun viewpagerDetailImageOnClickEvent() {
-//        binding.viewpagerDetailImage.setOnClickListener() {
-//
-//        }
-//    }
-//
-//    private fun checkImgDetailViewpagerPrevNext() {
-//        val viewpagerDetailImage = binding.viewpagerDetailImage
-//        val currentPageNumber = viewpagerDetailImage.currentItem
-//        Log.d("curPageNum", currentPageNumber.toString())
-//
-//        if(currentPageNumber == 0){
-//            binding.imgDetailViewpagerPrev.visibility = View.GONE
-//        } else if(currentPageNumber == detailViewPagerAdapter.itemCount - 1){
-//            binding.imgDetailViewpagerNext.visibility = View.GONE
-//        } else{
-//            binding.imgDetailViewpagerNext.visibility = View.VISIBLE
-//            binding.imgDetailViewpagerPrev.visibility = View.VISIBLE
-//        }
-//    }
-//
-//    private fun imgDetailViewpagerPrevOnClickEvent() {
-//        binding.imgDetailViewpagerPrev.setOnClickListener() {
-//
-//        }
-//    }
+    private fun test() {
+        binding.imgDetailParkingYes.isSelected = true
+        setTextColor(binding.tvDetailParkingYes, true)
+        binding.imgDetailParkingNo.isSelected = false
+
+        binding.imgDetailWarningHighway.isSelected = true
+        binding.imgDetailWarningMountain.isSelected = false
+        binding.imgDetailWarningBeginner.isSelected = true
+        binding.imgDetailWarningCrowded.isSelected = false
+    }
+
+    private fun setTextColor(textView: TextView, flag: Boolean) {
+        textView.setTextColor(Color.parseColor("#889098"))
+    }
+
+    private fun initDetailImageItem() {
+        binding.viewpagerDetailImage.adapter = detailViewPagerAdapter
+        detailViewPagerAdapter.imageList.addAll(LocalDetailViewpagerImageDataSource().fetchData())
+        detailViewPagerAdapter.notifyDataSetChanged()
+    }
+
+    private fun showDriveCourseInformationTextLenght() {
+        var text : String = binding.tvDetailInformationText.text.toString()
+        var textLength : Int = text.length
+        binding.tvDetailInformationTextLength.text = textLength.toString()+"/280자"
+    }
+
+    private fun imgDetailHeartOnClickEvent() {
+        binding.imgDetailHeart.setOnClickListener() {
+            val heart = binding.imgDetailHeart
+            if(heart.isSelected) {
+                Log.d("log", "heart is selected")
+                var likeCount = Integer.parseInt(binding.tvDetailHeartCount.text.toString()) - 1
+                binding.tvDetailHeartCount.text = likeCount.toString()
+            } else{
+                var likeCount = Integer.parseInt(binding.tvDetailHeartCount.text.toString()) + 1
+                binding.tvDetailHeartCount.text = likeCount.toString()
+            }
+            heart.isSelected = !heart.isSelected
+        }
+    }
+
+    private fun imgDetailSaveOnClickEvent(){
+        binding.imgDetailSave.setOnClickListener() {
+            val save = binding.imgDetailSave
+            if(save.isSelected){
+                Log.d("log", "save is selected")
+            }
+            save.isSelected = !save.isSelected
+        }
+    }
+
+    private fun imgDetailCopyOnClickEvent() {
+        binding.imgDetailMapStartCopy.setOnClickListener() {
+            var clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val address:String = binding.tvDetailMapStartAddress.text.toString()
+            val clip: ClipData = ClipData.newPlainText("Start Point Address", address)
+            clipboardManager.setPrimaryClip(clip)
+            Toast.makeText(this, "클립보드에 복사되었습니다.", Toast.LENGTH_LONG).show()
+        }
+        binding.imgDetailMapVia1Copy.setOnClickListener() {
+            var clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val address:String = binding.tvDetailMapVia1Address.text.toString()
+            val clip: ClipData = ClipData.newPlainText("Start Point Address", address)
+            clipboardManager.setPrimaryClip(clip)
+            Toast.makeText(this, "클립보드에 복사되었습니다.", Toast.LENGTH_LONG).show()
+        }
+        binding.imgDetailMapVia2Copy.setOnClickListener() {
+            var clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val address:String = binding.tvDetailMapVia2Address.text.toString()
+            val clip: ClipData = ClipData.newPlainText("Start Point Address", address)
+            clipboardManager.setPrimaryClip(clip)
+            Toast.makeText(this, "클립보드에 복사되었습니다.", Toast.LENGTH_LONG).show()
+        }
+        binding.imgDetailMapEndCopy.setOnClickListener() {
+            var clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val address:String = binding.tvDetailMapEndAddress.text.toString()
+            val clip: ClipData = ClipData.newPlainText("Start Point Address", address)
+            clipboardManager.setPrimaryClip(clip)
+            Toast.makeText(this, "클립보드에 복사되었습니다.", Toast.LENGTH_LONG).show()
+        }
+    }
 }
