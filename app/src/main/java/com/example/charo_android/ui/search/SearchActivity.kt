c83925b6fb7de388e520dc7184448dd0a1351969
ho-taek <kht072623@naver.com>
2021-07-09 05:31:09 +0900

    [FEAT] searchView - 뷰 구현

--- app/src/main/java/com/example/charo_android/ui/search/SearchActivity.kt
+++ app/src/main/java/com/example/charo_android/ui/search/SearchActivity.kt
@@ -10,10 +10,22 @@ import com.example.charo_android.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchBinding
+    val itemTheme = arrayOf("산", "바다", "호수","강","봄","여름","가을","겨울","해안도로","벚꽃","단풍","여유","스피드","야경","도심")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setContentView(R.layout.activity_search)
+
+
     }
+
+
+    private fun selectTheme(){
+        binding.btnSearchTheme.setOnClickListener {
+
+        }
+
+
+    }
+
 }
