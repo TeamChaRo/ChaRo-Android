25945b61c7bccf2203b0d286b4aa626736db61b2
hansh0101 <hansh0101@naver.com>
2021-07-11 01:19:17 +0900

    [FEAT] : Spinner Custom

--- app/src/main/java/com/example/charo_android/ui/charo/MyCharoFragment.kt
+++ app/src/main/java/com/example/charo_android/ui/charo/MyCharoFragment.kt
@@ -1,18 +1,15 @@
 package com.example.charo_android.ui.charo
 
-import android.app.Activity
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import com.example.charo_android.DetailViewpagerImageAdapter
+import androidx.navigation.fragment.findNavController
 import com.example.charo_android.R
 import com.example.charo_android.data.LocalMyCharoDataSource
 import com.example.charo_android.databinding.FragmentMyCharoBinding
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MyCharoFragment : Fragment() {
     private var _binding: FragmentMyCharoBinding? = null
@@ -42,7 +39,8 @@ class MyCharoFragment : Fragment() {
     private fun setUpSpinner() {
         val filter = resources.getStringArray(R.array.charo_filter)
         val adapter =
-            activity?.let { ArrayAdapter(it, R.layout.support_simple_spinner_dropdown_item, filter) }
+            activity?.let { ArrayAdapter(it, R.layout.item_charo_spinner, filter) }
+        adapter?.setDropDownViewResource(R.layout.item_charo_spinner)
         binding.spinnerMyCharoFilter.adapter = adapter
     }
 }
