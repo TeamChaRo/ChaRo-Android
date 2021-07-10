885a7900f9614656ca8e7bce37f6d4be71a787c9
hansh0101 <hansh0101@naver.com>
2021-07-10 19:13:54 +0900

    #25 [UI] 마이페이지 필터링용 Spinner 구현

--- app/src/main/java/com/example/charo_android/ui/charo/MyCharoFragment.kt
+++ app/src/main/java/com/example/charo_android/ui/charo/MyCharoFragment.kt
@@ -1,14 +1,18 @@
 package com.example.charo_android.ui.charo
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.charo_android.DetailViewpagerImageAdapter
 import com.example.charo_android.R
 import com.example.charo_android.data.LocalMyCharoDataSource
 import com.example.charo_android.databinding.FragmentMyCharoBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MyCharoFragment : Fragment() {
     private var _binding: FragmentMyCharoBinding? = null
@@ -23,6 +27,7 @@ class MyCharoFragment : Fragment() {
         val root: View = binding.root
 
         initMyCharoItem()
+        setUpSpinner()
 
         // Inflate the layout for this fragment
         return root
@@ -32,5 +37,12 @@ class MyCharoFragment : Fragment() {
         binding.recyclerviewMyCharo.adapter = myCharoAdapter
         myCharoAdapter.itemList.addAll(LocalMyCharoDataSource().fetchData())
         myCharoAdapter.notifyDataSetChanged()
+    }
+    
+    private fun setUpSpinner() {
+        val filter = resources.getStringArray(R.array.charo_filter)
+        val adapter =
+            activity?.let { ArrayAdapter(it, R.layout.support_simple_spinner_dropdown_item, filter) }
+        binding.spinnerMyCharoFilter.adapter = adapter
     }
 }
