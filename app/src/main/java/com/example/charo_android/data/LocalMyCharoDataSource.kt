25289f2599f14d51dd68e4da872fae7751c6e448
hansh0101 <hansh0101@naver.com>
2021-07-13 07:14:35 +0900

    #47 [FEAT] : LocalMyCharoDataSource.kt 주석 삭제

--- app/src/main/java/com/example/charo_android/data/LocalMyCharoDataSource.kt
+++ app/src/main/java/com/example/charo_android/data/LocalMyCharoDataSource.kt
@@ -17,18 +17,3 @@ class LocalMyCharoDataSource: MyCharoDataSource {
         )
     }
 }
-
-//package com.example.charo_android.data
-//
-//import com.example.charo_android.DetailViewpagerImageInfo
-//import com.example.charo_android.R
-//
-//class LocalDetailViewpagerImageDataSource: DetailViewpagerImageDataSource {
-//    override fun fetchData(): MutableList<DetailViewpagerImageInfo> {
-//        return mutableListOf(
-//            DetailViewpagerImageInfo(image = R.drawable.mask_group),
-//            DetailViewpagerImageInfo(image = R.drawable.mask_group_reverse),
-//            DetailViewpagerImageInfo(image = R.drawable.mask_group),
-//        )
-//    }
-//}
