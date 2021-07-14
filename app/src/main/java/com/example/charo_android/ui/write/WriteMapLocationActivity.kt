161c05b67621b178b577defaa39773dae198f3cb
hansh0101 <hansh0101@naver.com>
2021-07-14 20:33:00 +0900

    #56 [FEAT] : WriteMapLocationActivity 수정

--- app/src/main/java/com/example/charo_android/ui/write/WriteMapLocationActivity.kt
+++ app/src/main/java/com/example/charo_android/ui/write/WriteMapLocationActivity.kt
@@ -1,24 +1,28 @@
 package com.example.charo_android.ui.write
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
-import androidx.databinding.DataBindingUtil
 import com.example.charo_android.R
 import com.example.charo_android.databinding.ActivityWriteMapLocationBinding
-import com.skt.Tmap.TMapData
-import com.skt.Tmap.TMapMarkerItem
-import com.skt.Tmap.TMapPoint
-import com.skt.Tmap.TMapView
+import com.skt.Tmap.*
+import kotlin.concurrent.timer
+import java.lang.Exception
+import kotlin.concurrent.timerTask
 
 class WriteMapLocationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWriteMapLocationBinding
-    private val viewModel : WriteViewModel by viewModels()
+    private val viewModel: WriteViewModel by viewModels()
+    private lateinit var locationAddress: String
+    private lateinit var locationName: String
+    private lateinit var locationFlag: String
+    private lateinit var address: TMapAddressInfo
+    private var lat = 0.0
+    private var lon = 0.0
 
     val markerCount = 1
 
@@ -30,16 +34,16 @@ class WriteMapLocationActivity : AppCompatActivity() {
 //
 //        binding.lifecycleOwner = this
 //        binding.viewModel = viewModel
+        locationFlag = intent.getStringExtra("locationFlag").toString()
 
         binding.imgWriteMapLocationBack.setOnClickListener {
             onBackPressed()
-
         }
 
-        var resultLocation = intent.getStringExtra("setLocation").toString()
+        var resultLocation = intent.getStringExtra("text").toString()
 
-        Log.d("location", intent.getStringExtra("setLocation").toString())
-        binding.btnSetLocation.text = intent.getStringExtra("setLocation").toString()
+        Log.d("writeMapLocationActivity", intent.getStringExtra("text").toString())
+        binding.btnSetLocation.text = resultLocation
 
 
 //        var list = arrayOf<String>("","","","")
@@ -60,40 +64,131 @@ class WriteMapLocationActivity : AppCompatActivity() {
         tMapView.setSKTMapApiKey("")
         binding.clWriteMapLocationView.addView(tMapView)
 
-        val locationName = intent.getStringExtra("locationName")
-        val locationAddress = intent.getStringExtra("locationAddress")
-    //    var location = intent.getStringExtra("location")
+        if (locationFlag == "1") {
+            binding.imgWriteMapMarker.setImageResource(R.drawable.ic_route_start)
+        } else if (locationFlag == "4") {
+            binding.imgWriteMapMarker.setImageResource(R.drawable.ic_route_end)
+        } else {
+            binding.imgWriteMapMarker.setImageResource(R.drawable.ic_route_waypoint)
+        }
+        binding.imgWriteMapMarker.bringToFront()
+
+        locationName = intent.getStringExtra("locationName").toString()
+        locationAddress = intent.getStringExtra("locationAddress").toString()
+        //    var location = intent.getStringExtra("location")
 
         binding.textLocationName.text = locationName
         binding.textLocationAddress.text = locationAddress
 
         val tmapdata = TMapData()
-        tmapdata.findAllPOI(locationName){ poiItem ->
+        tmapdata.findAllPOI(locationName) { poiItem ->
             Log.d("poi", poiItem[0].poiPoint.toString())
 
-            val markerCurrentSpot = TMapMarkerItem()
             var tmapPointCurrentSpot = poiItem[0].poiPoint
-            tMapView.setCenterPoint(tmapPointCurrentSpot.longitude,tmapPointCurrentSpot.latitude);
-            val bitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.poi_dot)
-            markerCurrentSpot.icon = bitmap
-            markerCurrentSpot.setPosition(0.5F, 1.0F)
-            markerCurrentSpot.tMapPoint = tmapPointCurrentSpot
-            markerCurrentSpot.name = "currentSpot$markerCount"
-            tMapView.addMarkerItem("marker_current_spot$markerCount", markerCurrentSpot)
+            if (poiItem[0].upperAddrName != null) {
+                locationAddress = poiItem[0].upperAddrName
+                Log.d("address upper", poiItem[0].upperAddrName)
+            }
+            if (poiItem[0].middleAddrName != null) {
+                locationAddress += " " + poiItem[0].middleAddrName
+                Log.d("address middle", poiItem[0].middleAddrName)
+            }
+            if (poiItem[0].lowerAddrName != null) {
+                locationAddress += " " + poiItem[0].lowerAddrName
+                Log.d("address lower", poiItem[0].lowerAddrName)
+            }
+            if (poiItem[0].detailAddrName != null) {
+                locationAddress += " " + poiItem[0].detailAddrName
+                Log.d("address detail", poiItem[0].detailAddrName)
+            }
+            if (poiItem[0].firstNo != null) {
+                locationAddress += " " + poiItem[0].firstNo
+                Log.d("address firstNo", poiItem[0].firstNo)
+            }
+            if (poiItem[0].secondNo != null && poiItem[0].secondNo != "0") {
+                locationAddress += " " + poiItem[0].secondNo
+                Log.d("address secondNo", poiItem[0].secondNo)
+            }
+            tMapView.setCenterPoint(tmapPointCurrentSpot.longitude, tmapPointCurrentSpot.latitude)
+            lat = tmapPointCurrentSpot.latitude
+            lon = tmapPointCurrentSpot.longitude
+            address = tmapdata.reverseGeocoding(lat, lon, "A10")
+        }
 
+        val tmr = timer(period = 1000, initialDelay = 1000) {
+            lat = tMapView.centerPoint.latitude
+            lon = tMapView.centerPoint.longitude
+            if (tmapdata.reverseGeocoding(lat, lon, "A00") != null) {
+//                address = tmapdata.reverseGeocoding(lat, lon, "A00")
+                tmapdata.findAllPOI(
+                    tmapdata.reverseGeocoding(
+                        lat,
+                        lon,
+                        "A00"
+                    ).strFullAddress
+                ) { poiItem ->
+                    locationAddress = poiItem[0].name
+                }
+                if (tmapdata.reverseGeocoding(lat, lon, "A10").strRoadName != null) {
+                    locationName = tmapdata.reverseGeocoding(lat, lon, "A10").strRoadName
+                    Log.d("myLog", locationName)
+                }
+                if (tmapdata.reverseGeocoding(lat, lon, "A10").strBuildingName != null) {
+                    locationName = tmapdata.reverseGeocoding(lat, lon, "A10").strBuildingName
+                    Log.d("myLog", locationName)
+                }
+            }
 
-            binding.btnSetLocation.setOnClickListener {
-                val intent = Intent(this,WriteMapActivity::class.java)
-                intent.putExtra("locationName",locationName)
-                    .putExtra("pointLong",poiItem[0].poiPoint.longitude)
-                    .putExtra("pointLat",poiItem[0].poiPoint.latitude)
-                    .putExtra("resultLocation",resultLocation)
-
-                startActivity(intent)
-
+//            tmapdata.findAllPOI(address.strFullAddress) { poiItem ->
+//                locationName = poiItem[0].name
+//                locationAddress = poiItem[0].name
+//            }
+            runOnUiThread {
+                setLocationInfo()
             }
         }
 
+        binding.btnSetLocation.setOnClickListener {
+            tmr.cancel()
+            lat = tMapView.centerPoint.latitude
+            lon = tMapView.centerPoint.longitude
 
+            Log.d("test lat", lat.toString())
+            Log.d("test lon", lon.toString())
+            Log.d("test addr", address.strFullAddress)
+
+            val intent = Intent(this, WriteMapActivity::class.java)
+            intent.putExtra("textview", locationAddress)
+                .putExtra("pointLong", lon)
+                .putExtra("pointLat", lat)
+                .putExtra("locationFlag", locationFlag)
+
+            startActivity(intent)
+        }
+
+    }
+
+    private fun setLocationInfo() {
+        binding.textLocationAddress.text = locationAddress
+        binding.textLocationName.text = locationName
+    }
+
+    private fun btnSetLocationOnClickEvent(tMapView: TMapView) {
+        binding.btnSetLocation.setOnClickListener {
+            lat = tMapView.centerPoint.latitude
+            lon = tMapView.centerPoint.longitude
+
+            Log.d("test lat", lat.toString())
+            Log.d("test lon", lon.toString())
+            Log.d("test addr", address.strFullAddress)
+
+            val intent = Intent(this, WriteMapActivity::class.java)
+            intent.putExtra("textview", locationAddress)
+                .putExtra("pointLong", lon)
+                .putExtra("pointLat", lat)
+                .putExtra("locationFlag", locationFlag)
+
+            startActivity(intent)
+        }
     }
 }
