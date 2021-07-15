2891af3c576679c1fbf74c2d4837dbd5d09f0ad0
hansh0101 <hansh0101@naver.com>
2021-07-16 06:26:13 +0900

    #69 [FEAT] WriteMapLocationActivity 에러 수정

--- app/src/main/java/com/example/charo_android/hidden/Hidden.kt
+++ app/src/main/java/com/example/charo_android/hidden/Hidden.kt
@@ -1,9 +1,9 @@
 package com.example.charo_android.hidden
 
 class Hidden {
-    val tMapApiKey: String = ""
+    val tMapApiKey: String = "l7xx94a7679a3e1d41a782105327ae7af1cd"
 
     companion object {
-        const val baseUrl: String = ""
+        const val baseUrl: String = "http://3.139.62.132:5000"
     }
 }
