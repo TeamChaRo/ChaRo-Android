eca202db69bade1cbd90b31711276cdb1cad498a
hansh0101 <hansh0101@naver.com>
2021-07-16 04:14:00 +0900

    #56 [FEAT] WriteMapSearchActivity 구현

--- app/src/main/java/com/example/charo_android/ui/write/WriteMapSearchActivity.kt
+++ app/src/main/java/com/example/charo_android/ui/write/WriteMapSearchActivity.kt
@@ -1,30 +1,22 @@
 package com.example.charo_android.ui.write
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.view.KeyEvent
-import android.view.KeyEvent.KEYCODE_ENTER
-import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
 import com.example.charo_android.data.MapSearchInfo
 import com.example.charo_android.databinding.ActivityWriteMapSearchBinding
 import com.skt.Tmap.TMapData
-import com.skt.Tmap.TMapPOIItem
 
 class WriteMapSearchActivity : AppCompatActivity() {
 
-    lateinit var location: String
     private lateinit var binding: ActivityWriteMapSearchBinding
     private lateinit var writeMapSearchAdapter: WriteMapSearchAdapter
 
     private lateinit var locationFlag: String
-
-    var list = mutableListOf<MapSearchInfo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,143 +62,38 @@ class WriteMapSearchActivity : AppCompatActivity() {
 //        var list = listOf<MapSearchInfo>()
         //      val POIItem = tmapdata.findAllPOI("SKT타워")
 
-
-        //동기화 필요 //뒤로가기 이슈
-//        binding.etWriteMapSearchStart.setOnKeyListener { v, keyCode, event ->
-//            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KEYCODE_ENTER) {
-//                list = mutableListOf<MapSearchInfo>()
-//                tmapdata.findAllPOI(binding.etWriteMapSearchStart.text.toString()) { poiItem ->
-//                    for (i in 0 until poiItem.size) {
-//                        val item = poiItem[i]
-//
-//                        list.add(
-//                            MapSearchInfo(
-//                                locationName = item.poiName,
-//                                locationAddress = item.poiAddress.replace("null", "")
-//                            )
-//
-//                        )
-//                    }
-//                    writeMapSearchAdapter.userList = list
-//
-//                }
-//                writeMapSearchAdapter.notifyDataSetChanged()
-//
-//            }
-//
-//            true
-//        }
-
-        binding.etWriteMapSearchStart.setOnEditorActionListener { v, actionId, event ->
-            var handled = false
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                tmapdata.findAllPOI(binding.etWriteMapSearchStart.text.toString()) { poiItem ->
-                    list.clear()
-                    for (i in 0 until poiItem.size) {
-                        val item = poiItem[i]
-                        list.add(
-                            MapSearchInfo(
-                                locationName = item.poiName,
-                                locationAddress = item.poiAddress.replace("null", "")
-                            )
-                        )
-                    }
-                    writeMapSearchAdapter.userList = list
-
-                }
-                writeMapSearchAdapter.notifyDataSetChanged()
-                handled = true
+        binding.etWriteMapSearchStart.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("myLog", "before")
             }
-            handled
-        }
 
-//        binding.etWriteMapSearchStart.addTextChangedListener(object: TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//                Log.d("myLog", "before")
-////                list.clear()
-////                tmapdata.findAllPOI(binding.etWriteMapSearchStart.text.toString()) { poiItem ->
-////                    for (i in 0 until poiItem.size) {
-////                        val item = poiItem[i]
-////
-////                        list.add(
-////                            MapSearchInfo(
-////                                locationName = item.poiName,
-////                                locationAddress = item.poiAddress.replace("null", "")
-////                            )
-////
-////                        )
-////                    }
-////                    writeMapSearchAdapter.userList = list
-////
-////                }
-////                writeMapSearchAdapter.notifyDataSetChanged()
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                Log.d("myLog", "changing")
-////                list.clear()
-////                tmapdata.findAllPOI(binding.etWriteMapSearchStart.text.toString()) { poiItem ->
-////                    for (i in 0 until poiItem.size) {
-////                        val item = poiItem[i]
-////
-////                        list.add(
-////                            MapSearchInfo(
-////                                locationName = item.poiName,
-////                                locationAddress = item.poiAddress.replace("null", "")
-////                            )
-////
-////                        )
-////                    }
-////                    writeMapSearchAdapter.userList = list
-////
-////                }
-////                writeMapSearchAdapter.notifyDataSetChanged()
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {
-//                Log.d("myLog", "after")
-////                list.clear()
-////                Handler(Looper.getMainLooper()).postDelayed({
-////                    val info: TMapInfo = tMapView.getDisplayTMapInfo(inputMapPointList)
-////                    tMapView.setCenterPoint(info.tMapPoint.longitude, info.tMapPoint.latitude)
-////                    tMapView.zoomLevel = info.tMapZoomLevel
-////                }, 500)
-//                var list = mutableListOf<MapSearchInfo>()
-////                Handler(Looper.getMainLooper()).postDelayed({
-//                    tmapdata.findAllPOI(binding.etWriteMapSearchStart.text.toString()) { poiItem ->
-//                        for (i in 0 until poiItem.size) {
-//                            val item = poiItem[i]
-//
-//                            list.add(
-//                                MapSearchInfo(
-//                                    locationName = item.poiName,
-//                                    locationAddress = item.poiAddress.replace("null", "")
-//                                )
-//
-//                            )
-//                        }
-//                        writeMapSearchAdapter.userList = list
-//
-//                    }
-//                    writeMapSearchAdapter.notifyDataSetChanged()
-////                }, 500)
-////                tmapdata.findAllPOI(binding.etWriteMapSearchStart.text.toString()) { poiItem ->
-////                    for (i in 0 until poiItem.size) {
-////                        val item = poiItem[i]
-////
-////                        list.add(
-////                            MapSearchInfo(
-////                                locationName = item.poiName,
-////                                locationAddress = item.poiAddress.replace("null", "")
-////                            )
-////
-////                        )
-////                    }
-////                    writeMapSearchAdapter.userList = list
-////
-////                }
-////                writeMapSearchAdapter.notifyDataSetChanged()
-//            }
-//        })
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("myLog", "changing")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("myLog", "after")
+                var list = mutableListOf<MapSearchInfo>()
+                Handler(Looper.getMainLooper()).postDelayed({
+                    if(binding.etWriteMapSearchStart.text.isNotEmpty()) {
+                        tmapdata.findTitlePOI(binding.etWriteMapSearchStart.text.toString()) { poiItem ->
+                            for (i in 0 until poiItem.size) {
+                                val item = poiItem[i]
+
+                                list.add(
+                                    MapSearchInfo(
+                                        locationName = item.poiName,
+                                        locationAddress = item.poiAddress.replace("null", "")
+                                    )
+
+                                )
+                            }
+                            writeMapSearchAdapter.userList = list
+                        }
+                        writeMapSearchAdapter.notifyDataSetChanged()
+                    }
+                }, 500)
+            }
+        })
     }
 }
