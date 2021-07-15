bd4793fa69970c723c98c80da46b264fb5640a39
hansh0101 <hansh0101@naver.com>
2021-07-15 22:02:44 +0900

    #55 [FEAT] : MyPage 서버통신 구현

---
+++ app/src/main/java/com/example/charo_android/data/ResponseMyPageLikeData.kt
@@ -0,0 +1,15 @@
+package com.example.charo_android.data
+
+data class ResponseMyPageLikeData(
+    val data: Data,
+    val msg: String,
+    val success: Boolean
+) {
+    data class Data(
+        val savedPost: List<SavedPost>,
+        val savedTotal: Int,
+        val userInformation: UserInformation,
+        val writtenPost: List<WrittenPost>,
+        val writtenTotal: Int
+    )
+}
