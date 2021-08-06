ff7cdd5c8f2379bfa434750e8e57ffca59acc6ac
hansh0101 <hansh0101@naver.com>
2021-08-07 00:30:29 +0900

    #84 [CHORE]: gitignore 수정

--- buildSrc/src/main/java/Apps.kt
+++
@@ -1,8 +0,0 @@
-object Apps {
-    const val compileSdk = 30
-    const val minSdk = 24
-    const val targetSdk = 30
-    const val versionCode = 1
-    const val versionName = "1.0.0"
-    const val pacakageName = "com.hjinhee.android"
-}
