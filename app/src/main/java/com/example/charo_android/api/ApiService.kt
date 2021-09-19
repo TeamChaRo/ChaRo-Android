baa02b1becff1bd007b2b3af278a2a30a57dfb39
hansh0101 <hansh0101@naver.com>
2021-09-19 23:59:29 +0900

    #104 [FEAT] : 마이페이지 무한스크롤 API 연결

--- app/src/main/java/com/example/charo_android/api/ApiService.kt
+++ app/src/main/java/com/example/charo_android/api/ApiService.kt
@@ -23,7 +23,8 @@ object ApiService {
     val detailViewSaveService: DetailViewSaveService = retrofit.create(DetailViewSaveService::class.java)
     val moreViewNewService: MoreViewNewService = retrofit.create(MoreViewNewService::class.java)
     val searchViewService : SearchViewService = retrofit.create(SearchViewService::class.java)
-    val myPageViewSortedByPopularService: MyPageViewSortedByPopularService = retrofit.create(MyPageViewSortedByPopularService::class.java)
-    val myPageViewSortedByDateService: MyPageViewSortedByDateService = retrofit.create(MyPageViewSortedByDateService::class.java)
+    val myPageViewLikeService = retrofit.create(MyPageViewLikeService::class.java)
+    val myPageViewNewService = retrofit.create(MyPageViewNewService::class.java)
+    val myPageViewMoreService = retrofit.create(MyPageViewMoreService::class.java)
     val writeViewService: WriteViewService = retrofit.create(WriteViewService::class.java)
 }
