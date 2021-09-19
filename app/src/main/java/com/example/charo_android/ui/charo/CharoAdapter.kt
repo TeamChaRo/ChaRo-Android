956e643a1dd0b4465837f0976b178e0474fbb0a6
hansh0101 <hansh0101@naver.com>
2021-09-20 00:05:27 +0900

    #104 [FEAT] : 마이페이지 무한스크롤 어댑터 구성

--- app/src/main/java/com/example/charo_android/ui/charo/CharoAdapter.kt
+++ app/src/main/java/com/example/charo_android/ui/charo/CharoAdapter.kt
@@ -8,47 +8,20 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.charo_android.data.mypage.Drive
+import com.example.charo_android.databinding.ItemCharoLoadingBinding
 import com.example.charo_android.ui.main.MainActivity
 import com.example.charo_android.ui.detail.DetailActivity
 import com.example.charo_android.databinding.ItemCharoMyCharoBinding
 
 class CharoAdapter(
     val userId: String
-): RecyclerView.Adapter<CharoAdapter.MyCharoViewHolder>() {
-    var itemList = mutableListOf<Drive>()
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val VIEW_TYPE_ITEM = 0
+    private val VIEW_TYPE_LOADING = 1
+    var itemList = mutableListOf<Drive?>()
+    var spinnerPosition = 0
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyCharoViewHolder {
-        val binding = ItemCharoMyCharoBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false
-        )
-        return MyCharoViewHolder(binding)
-    }
-
-    override fun getItemCount(): Int {
-        Log.d("recycler view item size", itemList.size.toString())
-        return itemList.size
-    }
-
-    override fun onBindViewHolder(holder: MyCharoViewHolder, position: Int) {
-        holder.onBind(itemList[position])
-
-        holder.binding.root.setOnClickListener() {
-            val intent = Intent(holder.itemView.context, DetailActivity::class.java)
-            intent.putExtra("userId", userId)
-            intent.putExtra("postId", itemList[position].postId)
-            // 서버통신 하기 전에
-            // intent로 recyclerview item이
-            // 내가 선택한 item의 ~~로 intent가 잘 넘어가는지
-            // test -> O
-
-            ContextCompat.startActivity(holder.itemView.context, intent, null)
-            // startActivity 바꿔라.
-        }
-    }
-
-    class MyCharoViewHolder(
+    inner class MyCharoViewHolder(
         val binding: ItemCharoMyCharoBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         @SuppressLint("SetTextI18n")
@@ -58,5 +31,62 @@ class CharoAdapter(
                 binding.tvMyCharoTag3.visibility = ViewGroup.VISIBLE
             }
         }
+    }
+
+    inner class LoadingViewHolder(
+        val binding: ItemCharoLoadingBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+
+    override fun getItemViewType(position: Int): Int {
+        return if (itemList[position] != null) {
+            Log.d("itemList[position]", "$position -> not null")
+            VIEW_TYPE_ITEM
+        } else {
+            Log.d("itemList[position]", "$position -> null")
+            VIEW_TYPE_LOADING
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_ITEM -> {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemCharoMyCharoBinding.inflate(layoutInflater, parent, false)
+                MyCharoViewHolder(binding)
+            }
+            else -> {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemCharoLoadingBinding.inflate(layoutInflater, parent, false)
+                LoadingViewHolder(binding)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = itemList.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is MyCharoViewHolder) {
+            holder.onBind(itemList[position]!!)
+
+            holder.binding.root.setOnClickListener() {
+                val intent = Intent(holder.itemView.context, DetailActivity::class.java)
+                intent.putExtra("userId", userId)
+                intent.putExtra("postId", itemList[position]!!.postId)
+
+                ContextCompat.startActivity(holder.itemView.context, intent, null)
+                // startActivity 바꿔라.
+            }
+        }
+    }
+
+    fun removeLoading() {
+        if(itemList.last() == null) {
+            itemList.removeLast()
+        }
+    }
+
+    fun addLoading() {
+        itemList.add(null)
+        this.notifyItemRangeInserted(itemList.size, 1)
     }
 }
