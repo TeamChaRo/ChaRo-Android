45811f302a6b1720314f0a0819b9138e736baee1
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-09-30 18:53:27 +0900

    #95 [FEAT] : DetailView 서버통신 코드 구현

--- app/src/main/java/com/example/charo_android/data/api/detail/DetailViewService.kt
+++ app/src/main/java/com/example/charo_android/data/api/detail/DetailViewService.kt
@@ -1,13 +1,27 @@
 package com.example.charo_android.data.api.detail
 
+import com.example.charo_android.data.model.detail.RequestDetailLikeAndSave
 import com.example.charo_android.data.model.detail.ResponseDetailData
+import com.example.charo_android.data.model.detail.ResponseDetailLikeAndSave
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface DetailViewService {
     @GET("/post/detail/{userEmail}/{postId}")
     fun getDetail(
-        @Path("userEmail") userEmail: String, @Path("postId") postId: String
+        @Path("userEmail") userEmail: String, @Path("postId") postId: Int
     ): Call<ResponseDetailData>
+
+    @POST("/post/like")
+    fun postDetailLike(
+        @Body body: RequestDetailLikeAndSave
+    ): Call<ResponseDetailLikeAndSave>
+
+    @POST("/post/save")
+    fun postDetailSave(
+        @Body body: RequestDetailLikeAndSave
+    ): Call<ResponseDetailLikeAndSave>
 }
