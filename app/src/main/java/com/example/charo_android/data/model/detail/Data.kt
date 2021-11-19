bfd98bf9665e279d7a5f32efc4a532fbeb054c1f
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-11-20 03:46:53 +0900

    #131 [FEAT] : DetailView BottomSheetDialog RecyclerView DUMMY DATA test

---
+++ app/src/main/java/com/example/charo_android/data/model/detail/Data.kt
@@ -0,0 +1,8 @@
+package com.example.charo_android.data.model.detail
+
+data class Data(
+    val image: String,
+    val is_follow: Boolean,
+    val nickname: String,
+    val userEmail: String,
+)
