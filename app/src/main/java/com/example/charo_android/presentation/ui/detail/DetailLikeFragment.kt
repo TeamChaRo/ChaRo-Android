c90c4106826d6367402d3073b62555fb62bcf006
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-11-23 03:38:35 +0900

    #131 [FEAT] : implement server connection code

--- app/src/main/java/com/example/charo_android/presentation/ui/detail/DetailLikeFragment.kt
+++ app/src/main/java/com/example/charo_android/presentation/ui/detail/DetailLikeFragment.kt
@@ -3,15 +3,14 @@ package com.example.charo_android.presentation.ui.detail
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import com.example.charo_android.data.model.detail.Data
+import androidx.fragment.app.activityViewModels
 import com.example.charo_android.databinding.DialogDetailLikeBinding
-import com.example.charo_android.presentation.ui.main.MainActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -19,6 +18,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 class DetailLikeFragment : BottomSheetDialogFragment() {
     private var _binding: DialogDetailLikeBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: DetailViewModel by activityViewModels()
     private val adapter by lazy {
         DetailLikeAdapter() {
 //            val intent = Intent(context, MainActivity::class.java)
@@ -32,6 +32,12 @@ class DetailLikeFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = DialogDetailLikeBinding.inflate(layoutInflater, container, false)
+
+        if (viewModel.userData.value == null) {
+            Log.d("viewModel.userData", "execute")
+            viewModel.getLikes(viewModel.postId.value!!)
+        }
+
         return binding.root
     }
 
@@ -41,6 +47,9 @@ class DetailLikeFragment : BottomSheetDialogFragment() {
 
         initDialog()
         initRecyclerView()
+        viewModel.userData.observe(viewLifecycleOwner, {
+            addRecyclerViewItem()
+        })
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
@@ -54,23 +63,31 @@ class DetailLikeFragment : BottomSheetDialogFragment() {
     }
 
     private fun initRecyclerView() {
-        adapter.itemList.addAll(
-            listOf(
-                Data("", true, "한승현", "email"),
-                Data("", true, "한진희", "email"),
-                Data("", true, "곽호택", "email"),
-                Data("", false, "장혜령", "email"),
-                Data("", false, "박익범", "email"),
-                Data("", false, "이지원", "email"),
-                Data("", false, "최인정", "email"),
-                Data("", false, "오예원", "email"),
-                Data("", false, "황지은", "email"),
-                Data("", false, "배희진", "email"),
-                Data("", false, "고다빈", "email"),
-                Data("", false, "이성현", "email"),
-                Data("", false, "고수현", "email"),
-            )
-        )
+//        adapter.itemList.addAll(
+//            listOf(
+//                Data("한승현", "email", "", true,),
+//                Data("", true, "한진희", "email"),
+//                Data("", true, "곽호택", "email"),
+//                Data("", false, "장혜령", "email"),
+//                Data("", false, "박익범", "email"),
+//                Data("", false, "이지원", "email"),
+//                Data("", false, "최인정", "email"),
+//                Data("", false, "오예원", "email"),
+//                Data("", false, "황지은", "email"),
+//                Data("", false, "배희진", "email"),
+//                Data("", false, "고다빈", "email"),
+//                Data("", false, "이성현", "email"),
+//                Data("", false, "고수현", "email"),
+//            )
+//        )
+        binding.rcvDetailDialog.adapter = adapter
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun addRecyclerViewItem() {
+        if(viewModel.userData.value != null) {
+            adapter.itemList.addAll(viewModel.userData.value!!)
+        }
         binding.rcvDetailDialog.adapter = adapter
         adapter.notifyDataSetChanged()
     }
