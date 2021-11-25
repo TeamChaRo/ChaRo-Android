6675e1d16d328b095f57d72d0f533ea8a9dd9919
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-11-25 17:24:12 +0900

    #131 [FEAT] : DetailView BottomSheetDialog 구현

--- app/src/main/java/com/example/charo_android/presentation/ui/detail/DetailLikeFragment.kt
+++ app/src/main/java/com/example/charo_android/presentation/ui/detail/DetailLikeFragment.kt
@@ -9,7 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.activityViewModels
+import com.example.charo_android.data.model.detail.UserData
 import com.example.charo_android.databinding.DialogDetailLikeBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -47,16 +49,16 @@ class DetailLikeFragment : BottomSheetDialogFragment() {
 
         initDialog()
         initRecyclerView()
-        viewModel.userData.observe(viewLifecycleOwner, {
-            addRecyclerViewItem()
-        })
+//        viewModel.userData.observe(viewLifecycleOwner, {
+//            addRecyclerViewItem()
+//        })
         clickClose()
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
     private fun initDialog() {
         binding.clDetailDialog.layoutParams.height =
-            requireActivity().windowManager.currentWindowMetrics.bounds.height()
+            (requireActivity().windowManager.currentWindowMetrics.bounds.height() * 0.9).toInt()
         (dialog as BottomSheetDialog).behavior.apply {
             isFitToContents = false
             state = BottomSheetBehavior.STATE_HALF_EXPANDED
@@ -64,29 +66,29 @@ class DetailLikeFragment : BottomSheetDialogFragment() {
     }
 
     private fun initRecyclerView() {
-//        adapter.itemList.addAll(
-//            listOf(
-//                Data("한승현", "email", "", true,),
-//                Data("", true, "한진희", "email"),
-//                Data("", true, "곽호택", "email"),
-//                Data("", false, "장혜령", "email"),
-//                Data("", false, "박익범", "email"),
-//                Data("", false, "이지원", "email"),
-//                Data("", false, "최인정", "email"),
-//                Data("", false, "오예원", "email"),
-//                Data("", false, "황지은", "email"),
-//                Data("", false, "배희진", "email"),
-//                Data("", false, "고다빈", "email"),
-//                Data("", false, "이성현", "email"),
-//                Data("", false, "고수현", "email"),
-//            )
-//        )
+        adapter.itemList.addAll(
+            listOf(
+                UserData("한승현", "email", "", true),
+                UserData("한진희", "email", "", true),
+                UserData("곽호택", "email", "", true),
+                UserData("장혜령", "email", "", false),
+                UserData("박익범", "email", "", false),
+                UserData("이지원", "email", "", false),
+                UserData("최인정", "email", "", false),
+                UserData("오예원", "email", "", false),
+                UserData("황지은", "email", "", false),
+                UserData("배희진", "email", "", false),
+                UserData("고다빈", "email", "", false),
+                UserData("이성현", "email", "", false),
+                UserData("고수현", "email", "", false),
+            )
+        )
         binding.rcvDetailDialog.adapter = adapter
         adapter.notifyDataSetChanged()
     }
 
     private fun addRecyclerViewItem() {
-        if(viewModel.userData.value != null) {
+        if (viewModel.userData.value != null) {
             adapter.itemList.addAll(viewModel.userData.value!!)
         }
         binding.rcvDetailDialog.adapter = adapter
@@ -97,5 +99,31 @@ class DetailLikeFragment : BottomSheetDialogFragment() {
         binding.imgDetailDialogClose.setOnClickListener {
             requireActivity().onBackPressed()
         }
+    }
+
+    private fun changeTopColor() {
+//        binding.nscvMyCharo.setOnScrollChangeListener((NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+//            if (v.getChildAt(v.childCount - 1) != null) {
+//                if ((scrollY >= (v.getChildAt(v.childCount - 1).measuredHeight - v.measuredHeight)) &&
+//                    scrollY > oldScrollY
+//                ) {
+//                    Log.d("무한스크롤 최하단 도달", "도달완")
+//                    if (myCharoViewModel.isServerConnection.value == false && charoAdapter.itemList.isNotEmpty()) {
+//                        charoAdapter.addLoading()
+//                        when (spinnerPosition) {
+//                            0 -> {
+//                                myCharoViewModel.getMoreWrittenLikeData()
+//                            }
+//                            else -> {
+//                                myCharoViewModel.getMoreWrittenNewData()
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }))
+        binding.nscvDetailLike.setOnScrollChangeListener((NestedScrollView.OnScrollChangeListener {v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            
+        }))
     }
 }
