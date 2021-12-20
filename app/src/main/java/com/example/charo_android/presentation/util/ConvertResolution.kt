52e07f4f5bf75241dfad3bfa17f0367a1de40bd1
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-12-21 00:11:03 +0900

    #134 [FEAT] : dpToPx, pxToDp 구현

---
+++ app/src/main/java/com/example/charo_android/presentation/util/ConvertResolution.kt
@@ -0,0 +1,13 @@
+package com.example.charo_android.presentation.util
+
+import android.util.DisplayMetrics
+
+object ConvertResolution {
+    fun dpToPx(dp: Int): Int {
+        return dp * DisplayMetrics().densityDpi
+    }
+
+    fun pxToDp(px: Int): Int {
+        return px / DisplayMetrics().densityDpi
+    }
+}
