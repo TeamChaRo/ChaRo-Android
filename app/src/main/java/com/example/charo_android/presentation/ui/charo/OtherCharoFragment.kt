b24bc92900cd33b1e791b0a9921a16a19f8c2920
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-12-24 02:43:23 +0900

    #157 [FEAT] : OtherCharoFragment 구현

--- app/src/main/java/com/example/charo_android/presentation/ui/charo/OtherCharoFragment.kt
+++ app/src/main/java/com/example/charo_android/presentation/ui/charo/OtherCharoFragment.kt
@@ -6,18 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import com.example.charo_android.R
 import com.example.charo_android.databinding.FragmentOtherCharoBinding
+import com.example.charo_android.hidden.Hidden
 
 class OtherCharoFragment : Fragment() {
+    private val charoViewModel: CharoViewModel by activityViewModels()
     private var _binding: FragmentOtherCharoBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_other_charo, container, false)
-
+        charoViewModel.getInitOtherLikeData(Hidden.otherUserEmail)
+        charoViewModel.otherInformation.observe(viewLifecycleOwner, {
+            binding.otherPageData = charoViewModel
+        })
         return binding.root
     }
 
