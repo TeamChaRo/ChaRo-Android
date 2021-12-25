c74aa4abfeff93b313ad098e4a2655f311af238e
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-12-25 22:14:54 +0900

    #163 [FEAT] : followerInfo adapter에 전달

--- app/src/main/java/com/example/charo_android/presentation/ui/charo/CharoListFollowerFragment.kt
+++ app/src/main/java/com/example/charo_android/presentation/ui/charo/CharoListFollowerFragment.kt
@@ -1,18 +1,19 @@
 package com.example.charo_android.presentation.ui.charo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import com.example.charo_android.R
 import com.example.charo_android.databinding.FragmentCharoListFollowerBinding
 
 class CharoListFollowerFragment : Fragment() {
     private var _binding: FragmentCharoListFollowerBinding? = null
     private val binding get() = _binding!!
     private val charoViewModel: CharoViewModel by activityViewModels()
+    private val followAdapter = CharoListFollowAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +21,11 @@ class CharoListFollowerFragment : Fragment() {
     ): View? {
         _binding = FragmentCharoListFollowerBinding.inflate(layoutInflater, container, false)
         initRecyclerView()
+        charoViewModel.followData.observe(viewLifecycleOwner, {
+            followAdapter.itemList.addAll(it.follower)
+            Log.d("itemList.size", followAdapter.itemList.size.toString())
+            followAdapter.notifyDataSetChanged()
+        })
         return binding.root
     }
 
@@ -30,7 +36,6 @@ class CharoListFollowerFragment : Fragment() {
 
     private fun initRecyclerView() {
         with(binding) {
-            val followAdapter = CharoListFollowAdapter()
             recyclerViewCharoListFollower.adapter = followAdapter
             followAdapter.notifyDataSetChanged()
         }
