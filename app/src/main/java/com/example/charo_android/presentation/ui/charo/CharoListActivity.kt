3a955f2ad832a2f32e2a7f8ce2b9cabb24cec869
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-12-25 22:14:09 +0900

    #163 [FEAT] : intent extra 추가

--- app/src/main/java/com/example/charo_android/presentation/ui/charo/CharoListActivity.kt
+++ app/src/main/java/com/example/charo_android/presentation/ui/charo/CharoListActivity.kt
@@ -8,11 +8,13 @@ import com.example.charo_android.hidden.Hidden
 
 class CharoListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCharoListBinding
+    var myPageEmail: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCharoListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        myPageEmail = intent.getStringExtra("myPageEmail")
         openFragment()
     }
 
