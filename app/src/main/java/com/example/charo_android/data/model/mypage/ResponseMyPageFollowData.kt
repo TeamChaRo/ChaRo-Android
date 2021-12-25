596ecc46a10c9c96a4a710aa4f44c35cbf3489b9
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-12-25 22:18:27 +0900

    #163 [REFACTOR] : Data Class 이름 변경

--- app/src/main/java/com/example/charo_android/data/model/mypage/ResponseMyPageFollowData.kt
+++ app/src/main/java/com/example/charo_android/data/model/mypage/ResponseMyPageFollowData.kt
@@ -1,7 +1,7 @@
 package com.example.charo_android.data.model.mypage
 
 data class ResponseMyPageFollowData(
-    val data: Data,
+    val data: FollowData,
     val msg: String,
     val success: Boolean
 )
