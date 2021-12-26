d6449d91d501af1321a4a8bedc819859a4a951de
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-12-27 02:56:05 +0900

    #165 [REFACTOR] : Re-Packaging

--- app/src/main/java/com/example/charo_android/presentation/util/FragmentChange.kt
+++ app/src/main/java/com/example/charo_android/presentation/util/FragmentChange.kt
@@ -1,4 +1,4 @@
-package com.example.charo_android.presentation.ui.home
+package com.example.charo_android.presentation.util
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
