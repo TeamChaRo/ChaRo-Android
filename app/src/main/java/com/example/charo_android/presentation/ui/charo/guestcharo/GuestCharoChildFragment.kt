d03a230170c9a1e1d6c243605f66a9f96de43040
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2021-12-31 02:54:43 +0900

    #177 [FEAT] : GuestCharoChildFragment 구현

---
+++ app/src/main/java/com/example/charo_android/presentation/ui/charo/guestcharo/GuestCharoChildFragment.kt
@@ -0,0 +1,26 @@
+package com.example.charo_android.presentation.ui.charo.guestcharo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.charo_android.databinding.FragmentGuestCharoChildBinding
+
+class GuestCharoChildFragment : Fragment() {
+    private var _binding: FragmentGuestCharoChildBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentGuestCharoChildBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
