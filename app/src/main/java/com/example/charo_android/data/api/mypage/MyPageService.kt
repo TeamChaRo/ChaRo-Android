c76162a4510f4d04c50f3f39296b1eb3846fe1f6
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-28 20:25:39 +0900

    #185 [FEAT] : 무한스크롤 suspend fun 4개 추가

--- app/src/main/java/com/example/charo_android/data/api/mypage/MyPageService.kt
+++ app/src/main/java/com/example/charo_android/data/api/mypage/MyPageService.kt
@@ -1,7 +1,7 @@
 package com.example.charo_android.data.api.mypage
 
-import com.example.charo_android.data.model.mypage.ResponseLikePost
-import com.example.charo_android.data.model.mypage.ResponseNewPost
+import com.example.charo_android.data.model.mypage.ResponseEndlessScroll
+import com.example.charo_android.data.model.mypage.ResponseMyPage
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -9,10 +9,36 @@ interface MyPageService {
     @GET("user/myPage/like/{userEmail}")
     suspend fun getLikePost(
         @Path("userEmail") userEmail: String
-    ): ResponseLikePost
+    ): ResponseMyPage
 
     @GET("user/myPage/new/{userEmail}")
     suspend fun getNewPost(
         @Path("userEmail") userEmail: String
-    ): ResponseNewPost
+    ): ResponseMyPage
+
+    @GET("user/myPage/like/{userEmail}/write/{lastId}/{lastCount}")
+    suspend fun getMoreWrittenLikePost(
+        @Path("userEmail") userEmail: String,
+        @Path("lastId") lastId: Int,
+        @Path("lastCount") lastCount: Int
+    ): ResponseEndlessScroll
+
+    @GET("user/myPage/new/{userEmail}/write/{lastId}")
+    suspend fun getMoreWrittenNewPost(
+        @Path("userEmail") userEmail: String,
+        @Path("lastId") lastId: Int
+    ): ResponseEndlessScroll
+
+    @GET("user/myPage/like/{userEmail}/save/{lastId}/{lastCount}")
+    suspend fun getMoreSavedLikePost(
+        @Path("userEmail") userEmail: String,
+        @Path("lastId") lastId: Int,
+        @Path("lastCount") lastCount: Int
+    ): ResponseEndlessScroll
+
+    @GET("user/myPage/new/{userEmail}/save/{lastId}")
+    suspend fun getMoreSavedNewPost(
+        @Path("userEmail") userEmail: String,
+        @Path("lastId") lastId: Int
+    ): ResponseEndlessScroll
 }
