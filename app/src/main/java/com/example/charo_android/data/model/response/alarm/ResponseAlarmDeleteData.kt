7cadf12917ee7b9b0be97d8d023f63be69ed1e5f
HJinhee <h.wlsgml19@gmail.com>
2022-02-12 01:55:27 +0900

    [FEAT] #200 : Push Alarm - Swipe로 삭제하기 구현 (SwipeHelperCallback 로직 및 UI)
     - 스와이프 시 삭제 버튼 클릭 가능
     - 스와이프는 뷰의 반만큼만  가능
     - 스와이프 후 놓을 시 지정한 길이로 돌아옴(삭제버튼이 보일 정도로만)
     - 스와이프 고정된 상태에서 다른 뷰 클릭 시 스와이프 고정 풀기
     - 한 방향으로만 스와이프 가능

---
+++ app/src/main/java/com/example/charo_android/data/model/response/alarm/ResponseAlarmDeleteData.kt
@@ -0,0 +1,7 @@
+package com.example.charo_android.data.model.response.alarm
+
+data class ResponseAlarmDeleteData (
+    val msg: String,
+    val success: Boolean
+) {
+}
--- app/src/main/java/com/example/charo_android/presentation/ui/alarm/AlarmActivity.kt
+++ app/src/main/java/com/example/charo_android/presentation/ui/alarm/AlarmActivity.kt
@@ -3,6 +3,7 @@ package com.example.charo_android.presentation.ui.alarm
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import com.example.charo_android.data.api.ApiService
 import com.example.charo_android.data.model.response.alarm.ResponseAlarmListData
 import com.example.charo_android.databinding.ActivityAlarmBinding
@@ -16,6 +17,7 @@ class AlarmActivity : AppCompatActivity() {
     private lateinit var alarmViewModel: AlarmViewModel
 
     private lateinit var alarmAdapter: AlarmListAdapter
+    private lateinit var alarmSwipeHelperCallback: AlarmSwipeHelperCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +25,12 @@ class AlarmActivity : AppCompatActivity() {
         setContentView(binding.root)
         goHome()
         alarmViewModel = AlarmViewModel()
+
+        alarmSwipeHelperCallback = AlarmSwipeHelperCallback().apply {
+            setClamp(170f)
+        }
+        val itemTouchHelper = ItemTouchHelper(alarmSwipeHelperCallback)
+        itemTouchHelper.attachToRecyclerView(binding.rcvAlarmList)
 
         initRecyclerView()
     }
@@ -32,71 +40,14 @@ class AlarmActivity : AppCompatActivity() {
             alarmAdapter = AlarmListAdapter(){
                 //itemClick 이벤트 구현
             }
+
             rcvAlarmList.adapter = alarmAdapter
 //            alarmViewModel.getInitAlarmData()
-            Log.e("getAlarmList param", "${Hidden.otherUserEmail}")
-            val call: Call<ResponseAlarmListData> =
-                ApiService.alarmViewService.getAlarmList(Hidden.otherUserEmail)
-            call.enqueue(object : Callback<ResponseAlarmListData> {
-                override fun onResponse(
-                    call: Call<ResponseAlarmListData>,
-                    response: Response<ResponseAlarmListData>
-                ) {
-                    Log.e("getAlarmList param111", "${Hidden.otherUserEmail}")
 
-                    if (response.isSuccessful) {
-                        Log.d("server connect : Alarm", "success")
-                        Log.d("server connect : Alarm", "${response.body()}")
-                        val pushList = response.body()?.pushList
-
-                        if(pushList != null){
-                            //alarmViewModel 사용 시
-                            alarmViewModel._pushId.value = pushList.pushId
-                            alarmViewModel._pushCode.value = pushList.pushCode
-                            alarmViewModel._isRead.value = pushList.isRead
-                            alarmViewModel._token.value = pushList.token
-                            alarmViewModel._image.value = pushList.image
-                            alarmViewModel._title.value = pushList.title
-                            alarmViewModel._body.value = pushList.body
-                            alarmViewModel._month.value = pushList.month
-                            alarmViewModel._day.value = pushList.day
-
-
-                            //alarmListInfo 사용 시
-                            alarmAdapter.itemList.addAll(
-                                listOf<AlarmListInfo>(
-                                    AlarmListInfo(
-                                        20,
-                                        2,
-                                        0,
-                                        "and@naver.com",
-                                        "https://charo-image.s3.ap-northeast-2.amazonaws.com/dummy/profileImage/default2.jpeg",
-                                        "팔로잉",
-                                        "안드네 토끼양님이 회원님을 팔로우하기 시작했습니다.",
-                                        "09",
-                                        "12"
-                                    ),
-                                    AlarmListInfo(
-                                        pushList.pushId,
-                                        pushList.pushCode,
-                                        pushList.isRead,
-                                        pushList.token,
-                                        pushList.image,
-                                        pushList.title,
-                                        pushList.body,
-                                        pushList.month,
-                                        pushList.day
-                                    )
-                                )
-                            )
-                        }else{
-
-                        }
-
-                        Log.e("alarmAdapter", "${alarmAdapter.itemList}")
-                        Log.e("alarmAdapter", "${alarmAdapter.itemCount}")
-
-
+            rcvAlarmList.setOnClickListener {
+                alarmSwipeHelperCallback.removePreviousClamp(rcvAlarmList)
+                false
+            }
             getInitAlarmData()
         }
     }
