cc7a2142a890b1f74ccbb69aaa443e27bb23a27d
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-02-12 00:36:41 +0900

    #203 [FEAT] : DetailPostActivity 생성

---
+++ app/src/main/java/com/example/charo_android/presentation/ui/detailpost/DetailPostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.charo_android.presentation.ui.detailpost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.charo_android.R
+
+class DetailPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_post)
+    }
+}
