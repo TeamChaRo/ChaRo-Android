2ce2d4ff4648f72efba98826705cc363eab051f9
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-02-11 13:00:07 +0900

    #197 : Resolve Conflict

--- app/src/main/java/com/example/charo_android/data/api/write/WriteViewService.kt
+++ app/src/main/java/com/example/charo_android/data/api/write/WriteViewService.kt
@@ -11,48 +11,18 @@ interface WriteViewService {
     @Multipart
     @POST("/post/write")
     fun writePost(
-        @Part("userEmail") userEmail: RequestBody, //String
-        @Part("title") title: RequestBody,  //String
-        @Part("province") province: RequestBody,  //String
-        @Part("region") region: RequestBody,  //String
+//        @Part("userEmail") userEmail: RequestBody, //String
+//        @Part("title") title: RequestBody,  //String
+//        @Part("province") province: RequestBody,  //String
+//        @Part("region") region: RequestBody,  //String
         @Part warning: ArrayList<MultipartBody.Part>?,  //ArrayList<String>?
-        @Part theme: ArrayList<MultipartBody.Part>?,  //ArrayList<String>?
+        @Part theme: ArrayList<MultipartBody.Part>?,  //ArrayList<MultipartBody.Part>?
         @Part("isParking") isParking: Boolean?,  //Boolean
-        @Part("parkingDesc") parkingDesc: RequestBody,  //String
-        @Part("courseDesc") courseDesc: RequestBody,  //String
+//        @Part("parkingDesc") parkingDesc: RequestBody,  //String
+//        @Part("courseDesc") courseDesc: RequestBody,  //String
         @Part("course") course: ArrayList<HashMap<String, String>>?, //RequestWriteData.Course
-        @Part image: ArrayList<MultipartBody.Part>?
+        @Part image: ArrayList<MultipartBody.Part>?,
+        @PartMap stringData : HashMap<String, RequestBody>,
+//        @PartMap courseData : HashMap<String, ArrayList<HashMap<String,RequestBody>>>
     ): Call<ResponseWriteData>
 }
-
-//data class RequestWriteData(
-//    @SerializedName("course")
-//    val course: Course,
-//    @SerializedName("courseDesc")
-//    val courseDesc: String,
-//    @SerializedName("isParking")
-//    val isParking: Boolean,
-//    @SerializedName("parkingDesc")
-//    val parkingDesc: String,
-//    @SerializedName("province")
-//    val province: String,
-//    @SerializedName("region")
-//    val region: String,
-//    @SerializedName("theme")
-//    val theme: List<String>,
-//    @SerializedName("title")
-//    val title: String,
-//    @SerializedName("userId")
-//    val userId: String,
-//    @SerializedName("warning")
-//    val warning: List<Boolean>
-//) {
-//    data class Course(
-//        @SerializedName("address")
-//        val address: List<String>,
-//        @SerializedName("latitude")
-//        val latitude: List<String>,
-//        @SerializedName("longtitude")
-//        val longtitude: List<String>
-//    )
-//}
