9c9395f6b0264184d3821e53ad68de509c6d6f55
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-02-22 11:09:12 +0900

    #203 [UI] : 게시물 상세보기 뷰 UI 구현

--- app/src/main/java/com/example/charo_android/presentation/ui/detailpost/DetailPostActivity.kt
+++ app/src/main/java/com/example/charo_android/presentation/ui/detailpost/DetailPostActivity.kt
@@ -1,12 +1,25 @@
 package com.example.charo_android.presentation.ui.detailpost
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.charo_android.R
+import com.example.charo_android.databinding.ActivityDetailPostBinding
 
 class DetailPostActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailPostBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_post)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail_post)
+
+        initFragmentContainerView()
+    }
+
+    private fun initFragmentContainerView() {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.add(R.id.fcv_detail_post, DetailPostFragment())
+            .commit()
     }
 }
--- app/src/main/java/com/example/charo_android/presentation/ui/detailpost/DetailPostFragment.kt
+++ app/src/main/java/com/example/charo_android/presentation/ui/detailpost/DetailPostFragment.kt
@@ -5,56 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.charo_android.R
+import com.example.charo_android.databinding.FragmentDetailPostBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailPostFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailPostFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentDetailPostBinding? = null
+    private val binding get() = _binding ?: error("binding not initialized")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_post, container, false)
+        _binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_detail_post, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailPostFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailPostFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
