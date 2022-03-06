239e09410a463a8d05fef945f87a7e0dab3923cf
HJinhee <h.wlsgml19@gmail.com>
2022-03-06 22:45:28 +0900

    [REFACTOR] #215 : WriteView - 경로 검색 글자 설정  - 관련 검색어 텍스트 중 입력값과 동일한 글자만 파란색 적용  - ellipsize 추가 (글자 끝에 ...)

--- app/src/main/java/com/example/charo_android/presentation/ui/write/WriteMapSearchAdapter.kt
+++ app/src/main/java/com/example/charo_android/presentation/ui/write/WriteMapSearchAdapter.kt
@@ -20,23 +20,20 @@ class WriteMapSearchAdapter(
 //    val locationFlag: String, val text: String, val userId: String, val nickName: String
 ) : RecyclerView.Adapter<WriteMapSearchAdapter.MapSearchViewHolder>() {
 
-    /* 2. Adapter 는 ViewHolder 로 변경할 Data 를 가지고 있어야함 */
     var userList = mutableListOf<MapSearchInfo>()
-//    var text = activity.intent.getStringExtra("locationM")
 
-    // 3. Adapter 는 아이템마다 ViewHolder를 만드는 방법을 정의
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MapSearchViewHolder {
         val binding = ItemAutoSearchBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
         )
+
         return MapSearchViewHolder(
             binding
         )
     }
 
-    // 4. Adapter 는 전체 아이템의 수를 알아야 함
     override fun getItemCount(): Int = userList.size
 
 //    lateinit var mainActivity: MainActivity
@@ -45,31 +42,17 @@ class WriteMapSearchAdapter(
 //        mainActivity = context as MainActivity
 //    }
 
-    // 5. Adapter 는 ViewHolder 에 Data 를 전달하는 방법을 정의
     override fun onBindViewHolder(holder: MapSearchViewHolder, position: Int) {
         holder.onBind(userList[position])
-
 
         var writeShareActivity = holder.itemView.context as WriteShareActivity
         var sharedViewModel = ViewModelProvider(writeShareActivity).get(WriteSharedViewModel::class.java)
 
-
-//        var text = activity.intent.getStringExtra("locationM")
-
         holder.itemView.setOnClickListener {
-//            val intent = Intent(holder.itemView?.context,WriteMapLocationActivity::class.java)
-
-//            intent.putExtra("locationName",userList[position].locationName)
-//                .putExtra("locationAddress",userList[position].locationAddress)
-//                .putExtra("locationFlag", locationFlag)
-//                .putExtra("text", text)
-//                .putExtra("userId", userId)
-//                .putExtra("nickName", nickName)
-
             var lat = 0.0
             var lon = 0.0
             val tmapdata = TMapData()
-            tmapdata.findAllPOI(userList[position].locationName) { poiItem ->
+            tmapdata.findAllPOI(userList[position].locationName.toString()) { poiItem ->
                 Log.d("poi", poiItem[0].poiPoint.toString())
                 var tmapPointCurrentSpot = poiItem[0].poiPoint
 
@@ -77,17 +60,14 @@ class WriteMapSearchAdapter(
                 lon = tmapPointCurrentSpot.longitude
             }
             //검색어 저장
-            postSaveHistory(userList[position].locationName, userList[position].locationAddress
+            postSaveHistory(userList[position].locationName.toString(), userList[position].locationAddress
                 , lat, lon
             )
 
-            sharedViewModel.locationName.value = userList[position].locationName
+            sharedViewModel.locationName.value = userList[position].locationName.toString()
             sharedViewModel.locationAddress.value = userList[position].locationAddress
 
             writeShareActivity.replaceFragment(WriteMapLocationFragment.newInstance(), "writeMapLocation")
-
-
-//            ContextCompat.startActivity(holder.itemView.context, intent, null)
 
         }
     }
@@ -96,9 +76,7 @@ class WriteMapSearchAdapter(
         private val binding: ItemAutoSearchBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(mapSearchInfo: MapSearchInfo) {
-
             binding.mapData = mapSearchInfo
-
         }
     }
 
