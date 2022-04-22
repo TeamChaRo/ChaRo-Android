fcd9c752f422072c50441685ecceeaa7f74653f1
hansh0101 <hansh0101@naver.com>
2022-04-22 13:14:06 +0900

    #268 [FEAT] : WriteImgInfo 필드 추가

--- app/src/main/java/com/charo/android/data/model/write/WriteImgInfo.kt
+++ app/src/main/java/com/charo/android/data/model/write/WriteImgInfo.kt
@@ -4,4 +4,9 @@ import android.net.Uri
 
 data class WriteImgInfo (
     val imgUri: Uri,
-    )
+    // note(승현):
+    // 이미지 Uri 가 기기 로컬 저장소 안의 이미지 Uri 인지,
+    // 혹은 서버 DB 저장소 안의 이미지 Uri 인지에 따라서
+    // 멀티파트화 하는 방식이 달라지기 때문에 아래 필드를 추가하였음
+    val isFromLocal: Boolean
+)
