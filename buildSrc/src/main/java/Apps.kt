7f99a603edc5cc012615f422339261c2bb6a9cbd
HJinhee <h.wlsgml19@gmail.com>
2022-05-03 20:23:00 +0900

    DevRelease v0.9.2 - 공유하기 기능 추가

--- buildSrc/src/main/java/Apps.kt
+++ buildSrc/src/main/java/Apps.kt
@@ -2,7 +2,7 @@ object Apps {
     const val compileSdk = 30
     const val minSdk = 24
     const val targetSdk = 30
-    const val versionCode = 91
-    const val versionName = "0.9.1"
+    const val versionCode = 92
+    const val versionName = "0.9.2"
     const val pacakageName = "com.charo.android"
 }
