cf5398cb5a6a7705ecd7463294d1a8b7c657ba20
hansh0101 <hansh0101@naver.com>
2022-05-18 15:30:04 +0900

    #292 [FEAT] : ApiService interceptor 추가

--- app/src/main/java/com/charo/android/data/api/ApiService.kt
+++ app/src/main/java/com/charo/android/data/api/ApiService.kt
@@ -8,14 +8,22 @@ import com.charo.android.data.api.charo.MyPageViewNewService
 import com.charo.android.data.api.detail.DetailViewService
 import com.charo.android.data.api.write.WriteViewService
 import com.charo.android.hidden.Hidden
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiService {
     private const val BASE_URL = Hidden.baseUrl
 
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BODY)
+        })
+
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client.build())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
