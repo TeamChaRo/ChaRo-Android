638456ede09ab6a5f90095b57104e123ec6ae32d
HJinhee <h.wlsgml19@gmail.com>
2022-05-22 12:51:42 +0900

    DevRelease v0.9.3

--- buildSrc/src/main/java/Apps.kt
+++ buildSrc/src/main/java/Apps.kt
@@ -2,7 +2,7 @@ object Apps {
     const val compileSdk = 30
     const val minSdk = 24
     const val targetSdk = 30
-    const val versionCode = 92
-    const val versionName = "0.9.2"
+    const val versionCode = 93
+    const val versionName = "0.9.3"
     const val pacakageName = "com.charo.android"
 }
