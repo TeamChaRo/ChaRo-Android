96f63e4d998687253636f1f8588490ffc39eaa6e
hansh0101 <hansh0101@naver.com>
2022-06-22 21:36:44 +0900

    #305 [ADD] : 확인요망 주석 추가

--- app/src/main/java/com/charo/android/domain/usecase/CoroutineUseCase.kt
+++ app/src/main/java/com/charo/android/domain/usecase/CoroutineUseCase.kt
@@ -3,6 +3,7 @@ package com.charo.android.domain.usecase
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
+// 확인요망
 abstract class CoroutineUseCase<P, R>(
     private val coroutineDispatcher: CoroutineDispatcher
 ) {
