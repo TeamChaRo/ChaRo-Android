a9dcff61414fb00b081fc0863eba3787b54f5482
HJinhee <h.wlsgml19@gmail.com>
2022-07-04 22:51:59 +0900

    [FEAT] : 배너 드라이브코스 바로가기 구현

--- app/src/main/java/com/charo/android/presentation/ui/home/BannerDriveTheaterActivity.kt
+++ app/src/main/java/com/charo/android/presentation/ui/home/BannerDriveTheaterActivity.kt
@@ -1,9 +1,11 @@
 package com.charo.android.presentation.ui.home
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
@@ -11,8 +13,9 @@ import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.charo.android.R
 import com.charo.android.databinding.ActivityBannerDriveTheaterBinding
+import com.charo.android.presentation.ui.write.WriteShareActivity
 
-class BannerDriveTheaterActivity : AppCompatActivity() {
+class BannerDriveTheaterActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityBannerDriveTheaterBinding
 
     private lateinit var requestManager : RequestManager
@@ -26,6 +29,8 @@ class BannerDriveTheaterActivity : AppCompatActivity() {
         requestManager = Glide.with(this)
         glideImg(resources.getDrawable(R.drawable.image_121), binding.ivPaju1)
 
+        binding.tvBtnBannerTheaterPajuPost.setOnClickListener(this)
+        binding.tvBtnBannerTheaterGangPost.setOnClickListener(this)
     }
 
     private fun glideImg(img : Drawable, imageView : ImageView){
@@ -51,5 +56,21 @@ class BannerDriveTheaterActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun goDriveCourse(postId : Int){
+        val intent = Intent(this, WriteShareActivity::class.java)
+        intent.apply {
+            putExtra("destination", "detail")
+            putExtra("postId", postId)
+        }
+        startActivity(intent)
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            binding.tvBtnBannerTheaterPajuPost -> goDriveCourse(41)
+            binding.tvBtnBannerTheaterGangPost -> goDriveCourse(27)
+        }
     }
 }
