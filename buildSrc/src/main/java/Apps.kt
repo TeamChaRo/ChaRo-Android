71f51184a69b1b5591d1a1607da855b299bdd7b0
hansh0101 <hansh0101@naver.com>
2022-10-02 16:36:06 +0900

    릴리즈 설정

--- buildSrc/src/main/java/Apps.kt
+++ buildSrc/src/main/java/Apps.kt
@@ -2,7 +2,7 @@ object Apps {
     const val compileSdk = 31
     const val minSdk = 24
     const val targetSdk = 31
-    const val versionCode = 93
-    const val versionName = "0.9.3"
+    const val versionCode = 95
+    const val versionName = "0.9.5"
     const val pacakageName = "com.charo.android"
 }
